2a41049b7895330256e9b16b51f4425133f79d62
Abhisar Garg <garg.abhisar@gmail.com>
2026-01-31 23:04:18 +0530

    Commented this for HTTP/IIS

--- BookInventory/Program.cs
+++ BookInventory/Program.cs
@@ -58,7 +58,9 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//Commented this for HTTP/IIS
+//app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
